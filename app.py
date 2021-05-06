03175712313089eff123cfa4aec5a6d73552ff81
jing20201111 <74511616jing20201111@users.noreply.github.com>
2021-05-06 12:48:38 +1200

    jing

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask,url_for,session, redirect, flash,render_template,request
+from flask import Flask,url_for,session, redirect, flash,render_template,request, send_from_directory
 import datetime as dt
 import time
 import psycopg2
@@ -10,6 +10,13 @@ from datetime import datetime, timedelta, date
 from dateutil.relativedelta import *
 from flask_mail import Mail, Message
 import smtplib
+import xlrd, xlwt, xlutils
+import os
+from werkzeug.utils import secure_filename
+from openpyxl import load_workbook
+import pandas as pd
+import numpy
+
 
 
 app = Flask(__name__)
@@ -27,9 +34,39 @@ def getCursor():
         return dbconn
 
 
-@app.route("/")
+@app.route("/", methods = ['POST','GET'])
 def index():
-    pass
+    return render_template("index.html")
+@app.route("/member", methods = ['POST','GET'])
+def member():
+    
+    return render_template("member.html")
+
+    
+@app.route("/member_upload", methods = ['POST','GET'])
+def member_upload():
+    if request.method =='POST':
+        file = request.files['file']
+        form = request.form
+        print(form,'FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF')
+        print(file,'llllllllllllllllllllllllllllllllllllllllllllllllllllllll')
+        if form:
+            return redirect(url_for('member'))
+            
+        else:
+            filename = secure_filename(file.filename)
+            basepath  = os.path.dirname(__file__)
+            excelpath = os.path.join(basepath,'uploads',filename)
+            file.save(excelpath)
+            wb = pd.read_excel(excelpath)
+            data = wb.iloc[4:].values
+            return render_template('member_upload.html',data = data)
+            
+            
+    
+       
+    
+
 
 
 if __name__ == '__main__':

9620b1234dbb3e0beb0b3c57679c9a3b39ba8083
jing20201111 <74511616jing20201111@users.noreply.github.com>
2021-06-10 14:18:51 +1200

    fix bugs

--- app.py
+++ app.py
@@ -444,10 +444,10 @@ def school_upload():
     if form:
         for i in range(0, len(form)-1):
             school_info = request.form.getlist(f'school{i}')
-            test(school_info[17])
+            school_info.pop(16)
             school_obj = schools_info.school_obj(school_info[:-1])
             school_obj.insert_db()
-        return redirect(url_for('school'))
+        return redirect(url_for('sch'))
     #  read uploaded excel file and send info to client-side
     else:
         excelpath = upload_path('file')
